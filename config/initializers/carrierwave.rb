4fadd276e5eabe218d0801f54cce06adad40444b
sketch25 <sket.ch.life725@gmail.com>
2020-09-02 22:45:48 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'fleamarket78a'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
   else
