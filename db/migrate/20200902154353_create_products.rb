7fcb0842840f67cdf83f1b332026f930a86b72fc
Hiroki2728 <moxx.jh@gmail.com>
2020-09-05 22:36:40 +0900

    product&categorysテーブルにカラム追加
    migrateはまだしてない。

--- db/migrate/20200902154353_create_products.rb
+++ db/migrate/20200902154353_create_products.rb
@@ -1,6 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+      t.string :name, null: false
+      t.references :user, foreign_key: true
+      t.text :details
+      t.string :condition
+      t.string :fee_side
+      t.string :origin
+      t.string :days
+      t.integer :price, null: false
 
       t.timestamps
     end
