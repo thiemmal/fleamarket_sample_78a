d9d6e26b15326f5f6bcb183a12f658a3ff178c2b
Hiroki2728 <moxx.jh@gmail.com>
2020-09-05 21:20:24 +0900

    usersテーブルにカラムを追加
    migrateはまだしてない。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_171435) do
+ActiveRecord::Schema.define(version: 2020_09_04_165556) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_09_04_171435) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
