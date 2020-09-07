6f7269a0785a912c85acccf548ed62a42d8e5c05
garaga-qaz <67787393+garaga-qaz@users.noreply.github.com>
2020-09-08 00:26:59 +0900

    商品詳細２

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  root 'products#show'
+  resources :products
+  # root 'products#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
