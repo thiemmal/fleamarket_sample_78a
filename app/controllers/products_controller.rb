85263cf6281d271d89992ee2276273d8c99d48e6
Hiroki2728 <moxx.jh@gmail.com>
2020-09-20 17:40:04 +0900

    user登録に関するルーティング、コントローラーの記載

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     
   end
@@ -9,5 +11,12 @@ class ProductsController < ApplicationController
 
   def edit
     
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only:[:show,:edit]
+  resources :users, only: [:edit, :update, :index]
 end
