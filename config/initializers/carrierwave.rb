dd9cdc59225473c7ba2ef772259c11275d28214b
sketch25 <sket.ch.life725@gmail.com>
2020-10-23 15:25:48 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
@@ -9,6 +13,9 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
+
+    config.fog_directory  = 'fleamarket78a'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket78a'
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
