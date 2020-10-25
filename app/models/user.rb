3eacf0d0ded836583e410a5ccf9cedabf5dfb6c7
Hiroki2728 <moxx.jh@gmail.com>
2020-10-25 17:35:12 +0900

    ユーザー登録のバリデーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   validates :email, format:{ with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/}
   validates :first_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
   validates :family_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
-  validates :first_furigana, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :family_furigana, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :first_furigana, format: { with: /\A([ぁ-ん]|ー)+\z/}
+  validates :family_furigana, format: { with: /\A([ぁ-ん]|ー)+\z/}
   validates :birthday, format: { with: /\A\d{8}\z/}
   validates :recipient_family_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
   validates :recipient_first_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
-  validates :recipient_family_furigana, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :recipient_first_furigana, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :zip_cord, format: { with: /\A\d{3}[-]\d{4}\z/}
+  validates :recipient_family_furigana, format: { with: /\A([ぁ-ん]|ー)+\z/}
+  validates :recipient_first_furigana, format: { with: /\A([ぁ-ん]|ー)+\z/}
+  validates :zip_cord, format: { with: /\A\d{7}\z/}
   validates :prefecture, presence: true
   validates :city, presence: true
   validates :address, presence: true
