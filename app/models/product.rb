768336190bf58b0ab1a4952d7be399adf41d7882
Hiroki2728 <moxx.jh@gmail.com>
2020-10-25 17:36:22 +0900

    商品モデルにカテゴリーを外部キー設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
-  # belongs_to :category
+  belongs_to :category, foreign_key: 'category_id'
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
