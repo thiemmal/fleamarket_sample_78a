d9427a011ed7be3bce2de848ab23f771924a97e8
garaga-qaz <67787393+garaga-qaz@users.noreply.github.com>
2020-10-30 21:03:34 +0900

    修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -45,27 +45,6 @@ class CardsController < ApplicationController
       customer = Payjp::Customer.retrieve(@card.customer_id)
       # カスタマー情報からカードの情報を引き出す
       @customer_card = customer.cards.retrieve(@card.card_id)
-
-      ##カードのアイコン表示のための定義づけ
-      # @card_brand = @customer_card.brand
-      # case @card_brand
-      # when "Visa"
-      #   # 例えば、Pay.jpからとってきたカード情報の、ブランドが"Visa"だった場合は返り値として
-      #   # (画像として登録されている)Visa.pngを返す
-      #   @card_src = "visa.png"
-      # when "JCB"
-      #   @card_src = "jcb.png"
-      # when "MasterCard"
-      #   @card_src = "master.png"
-      # when "American Express"
-      #   @card_src = "amex.png"
-      # when "Diners Club"
-      #   @card_src = "diners.png"
-      # when "Discover"
-      #   @card_src = "discover.png"
-      # end
-
-      #  viewの記述を簡略化
       ## 有効期限'月'を定義
       @exp_month = @customer_card.exp_month.to_s
       ## 有効期限'年'を定義
@@ -90,38 +69,10 @@ class CardsController < ApplicationController
       # 削除が完了しているか判断
       if @card.destroy
       # 削除完了していればdestroyのビューに移行
-      # destroyビューを作るのが面倒であれば、flashメッセージを入れてトップページやマイページに飛ばしてもOK
-
       else
         # 削除されなかった場合flashメッセージを表示させて、showのビューに移行
         redirect_to card_path(current_user.id), alert: "削除できませんでした。"
       end
     end
   end
-
-  # def new
-  #   @card = Card.new
-  # end
-
-  
-
-  # def create
-  #   Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
-  #   if params['payjp-token'].blank?
-  #     redirect_to action: "new"
-  #   else
-  #     user_id = current_user.id
-  #     customer = Payjp::Customer.create(
-  #     card: params['payjp-token']
-  #     ) 
-  #     redirect_to root_path
-  #     @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
-  #     if @card.save
-  #       redirect_to controller: :users, action: :done
-  #     else
-  #       redirect_to action: "new"
-  #     end
-  #   end
-  # end
-
 end
