e75ef1202298aa5c5af2fc9dd76596341fa9fd0e
garaga-qaz <67787393+garaga-qaz@users.noreply.github.com>
2020-10-30 20:35:13 +0900

    touroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_103048) do
+ActiveRecord::Schema.define(version: 2020_10_25_092450) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -20,9 +20,6 @@ ActiveRecord::Schema.define(version: 2020_10_18_103048) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
-
-# ActiveRecord::Schema.define(version: 2020_10_11_093738) do
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -57,13 +54,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_103048) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
-    t.integer "category_id"
-
     t.integer "prefecture_id"
     t.integer "condition_id"
     t.integer "days_id"
-
+    t.integer "category_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
